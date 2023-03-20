37745f2bf474e842205ede4a996f5de220d9b590
ViLsonCake <timofeydmitrenko13@gmail.com>
2023-03-20 17:59:38 +0100

    init project

---
+++ CubeTimerApplication.java
@@ -0,0 +1,13 @@
+package com.project.CubeTimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubeTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubeTimerApplication.class, args);
+	}
+
+}
